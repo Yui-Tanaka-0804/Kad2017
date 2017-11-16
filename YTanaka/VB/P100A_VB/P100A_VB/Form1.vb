f1bd167ae7175019eb830524fa87493c9a56683c
yTanaka <pirori.nyanko.033014@gmail.com>
2017-11-16 19:14:37 +0900

    VB削除

--- YTanaka/VB/P100A_VB/P100A_VB/Form1.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-        Label1.Text = "はじめての Visual Basic .NET プログラミング"
-
-    End Sub
-End Class
